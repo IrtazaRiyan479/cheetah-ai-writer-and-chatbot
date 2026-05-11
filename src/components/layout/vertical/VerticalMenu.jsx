ea905f30111feff3d7054521ac64e7ffdd772fa0
Irtaza Riyan <martinirtaza@gmail.com>
2026-05-11 15:15:02 +0500

    feat: build complete navigation sidebar and remove unused horizontal menu codebase

--- src/components/layout/vertical/VerticalMenu.jsx
+++ src/components/layout/vertical/VerticalMenu.jsx
@@ -5,6 +5,8 @@ import { useTheme } from '@mui/material/styles'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
 // Component Imports
+import verticalMenuData from '@/data/navigation/verticalMenuData'
+
 import { Menu, MenuItem } from '@menu/vertical-menu'
 
 // Hook Imports
@@ -16,6 +18,9 @@ import StyledVerticalNavExpandIcon from '@menu/styles/vertical/StyledVerticalNav
 // Style Imports
 import menuItemStyles from '@core/styles/vertical/menuItemStyles'
 import menuSectionStyles from '@core/styles/vertical/menuSectionStyles'
+
+// Generate Menu Imports
+import { GenerateVerticalMenu } from '@components/GenerateMenu'
 
 const RenderExpandIcon = ({ open, transitionDuration }) => (
   <StyledVerticalNavExpandIcon open={open} transitionDuration={transitionDuration}>
@@ -30,21 +35,14 @@ const VerticalMenu = ({ scrollMenu }) => {
 
   // Vars
   const { isBreakpointReached, transitionDuration } = verticalNavOptions
-  const ScrollWrapper = isBreakpointReached ? 'div' : PerfectScrollbar
+  const ScrollWrapper = 'div'
 
   return (
     // eslint-disable-next-line lines-around-comment
     /* Custom scrollbar instead of browser scroll, remove if you want browser scroll only */
     <ScrollWrapper
-      {...(isBreakpointReached
-        ? {
-            className: 'bs-full overflow-y-auto overflow-x-hidden',
-            onScroll: container => scrollMenu(container, false)
-          }
-        : {
-            options: { wheelPropagation: false, suppressScrollX: true },
-            onScrollY: container => scrollMenu(container, true)
-          })}
+      className='bs-full overflow-y-auto overflow-x-hidden [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden'
+      onScroll={container => scrollMenu(container, false)}
     >
       {/* Incase you also want to scroll NavHeader to scroll with Vertical Menu, remove NavHeader from above and paste it below this comment */}
       {/* Vertical Menu */}
@@ -55,22 +53,8 @@ const VerticalMenu = ({ scrollMenu }) => {
         renderExpandedMenuItemIcon={{ icon: <i className='ri-circle-fill' /> }}
         menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
       >
-        <MenuItem href='/home' icon={<i className='ri-home-smile-line' />}>
-          Home
-        </MenuItem>
-        <MenuItem href='/about' icon={<i className='ri-information-line' />}>
-          About
-        </MenuItem>
+        <GenerateVerticalMenu menuData={verticalMenuData()} />
       </Menu>
-      {/* <Menu
-          popoutMenuOffset={{ mainAxis: 17 }}
-          menuItemStyles={menuItemStyles(verticalNavOptions, theme)}
-          renderExpandIcon={({ open }) => <RenderExpandIcon open={open} transitionDuration={transitionDuration} />}
-          renderExpandedMenuItemIcon={{ icon: <i className='ri-circle-fill' /> }}
-          menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
-        >
-          <GenerateVerticalMenu menuData={menuData(dictionary, params)} />
-        </Menu> */}
     </ScrollWrapper>
   )
 }
--- src/data/navigation/verticalMenuData.jsx
+++ src/data/navigation/verticalMenuData.jsx
@@ -2,12 +2,52 @@ const verticalMenuData = () => [
   {
     label: 'Home',
     href: '/home',
-    icon: 'ri-home-smile-line'
+    icon: 'ri-home-smile-fill'
   },
   {
-    label: 'About',
-    href: '/about',
-    icon: 'ri-information-line'
+    label: 'CheetahChat',
+    href: '/chat',
+    icon: 'ri-chat-ai-2-fill'
+  },
+  {
+    label: 'CheetahWriter',
+    href: '/writer',
+    icon: 'ri-edit-box-fill'
+  },
+  {
+    label: 'CheetahImages',
+    href: '/images',
+    icon: 'ri-image-ai-fill'
+  },
+  {
+    label: 'CheetahLinks',
+    href: '/links',
+    icon: 'ri-links-line'
+  },
+  {
+    label: 'CheetahMagnets',
+    href: '/magnets',
+    icon: 'ri-book-marked-fill'
+  },
+  {
+    label: 'Pricing',
+    href: '/pricing',
+    icon: 'ri-rocket-2-fill'
+  },
+  {
+    label: 'Login',
+    href: '/login',
+    icon: 'ri-git-repository-private-fill'
+  },
+  {
+    label: 'Register',
+    href: '/register',
+    icon: 'ri-user-add-fill'
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+    icon: 'ri-megaphone-fill'
   }
 ]
 
