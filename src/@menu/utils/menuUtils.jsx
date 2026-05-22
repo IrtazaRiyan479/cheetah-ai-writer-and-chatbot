dc98f02e8fa2f527d78b7babce5ae0f77e246b03
Irtaza Riyan <martinirtaza@gmail.com>
2026-05-22 19:51:34 +0500

    refactor: remove unused views, dependencies, and configuration
    - Delete unused folders from src/views
    - Remove deprecated libraries from package.json
    - Update searchData to reflect current project routes
    - Cleanup unused assets and utility files

--- src/@menu/utils/menuUtils.jsx
+++ src/@menu/utils/menuUtils.jsx
@@ -1,12 +1,6 @@
 // React Imports
 import { Children, isValidElement } from 'react'
 
-// Component Imports
-import {
-  SubMenu as HorizontalSubMenu,
-  MenuItem as HorizontalMenuItem,
-  Menu as HorizontalMenu
-} from '../horizontal-menu'
 import { SubMenu as VerticalSubMenu, MenuItem as VerticalMenuItem, Menu as VerticalMenu } from '../vertical-menu'
 import { GenerateVerticalMenu } from '@components/GenerateMenu'
 
--- src/components/GenerateMenu.jsx
+++ src/components/GenerateMenu.jsx
@@ -5,7 +5,6 @@ import { useParams } from 'next/navigation'
 import Chip from '@mui/material/Chip'
 
 // Component Imports
-import { SubMenu as HorizontalSubMenu, MenuItem as HorizontalMenuItem } from '@menu/horizontal-menu'
 import { SubMenu as VerticalSubMenu, MenuItem as VerticalMenuItem, MenuSection } from '@menu/vertical-menu'
 
 // Util Imports
